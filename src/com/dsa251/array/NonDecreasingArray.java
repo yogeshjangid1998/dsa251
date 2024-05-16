76f4b68e79b38ae9e59a4e5e7544159568a6b022
yogeshjangid1998 <jangidyogesh023@gmail.com>
2024-05-16 10:21:13 +0530

    adding first question

---
+++ src/com/dsa251/array/NonDecreasingArray.java
@@ -0,0 +1,25 @@
+package com.dsa251.array;
+
+public class NonDecreasingArray {
+	
+//	link :- https://www.naukri.com/code360/problems/non-decreasing-array_699920?utm_source=youtube&utm_medium=affiliate&utm_campaign=parikh_youtube&leftPanelTabValue=PROBLEM
+
+	public static boolean isPossible(int[] arr, int n) {
+		// Write your code here.
+		int req = 0;
+		for(int i = 0; i < n-1; i++){
+			
+			if(arr[i] > arr[i+1]){
+				req++;
+				if(i > 0 && arr[i+1] < arr[i-1]){
+					arr[i+1] = arr[i];
+				}else{
+					arr[i] = arr[i+1];
+				}
+			}
+
+			if(req > 1) return false;
+		}
+		return true;
+	}
+}
